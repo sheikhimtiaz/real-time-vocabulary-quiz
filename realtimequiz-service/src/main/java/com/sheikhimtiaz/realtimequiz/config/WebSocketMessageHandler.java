971bd5c1ae3165f8464b33c3755912ce52870d9f
Sheikh Imtiaz Ahmed <imticorei54@gmail.com>
2024-12-18 11:22:48 +0600

    small refactor

--- realtimequiz-service/src/main/java/com/sheikhimtiaz/realtimequiz/config/WebSocketMessageHandler.java
+++ realtimequiz-service/src/main/java/com/sheikhimtiaz/realtimequiz/config/WebSocketMessageHandler.java
@@ -1,21 +1,29 @@
 package com.sheikhimtiaz.realtimequiz.config;
 
+import com.sheikhimtiaz.realtimequiz.service.QuizService;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import java.io.IOException;
-
 @Component
 public class WebSocketMessageHandler extends TextWebSocketHandler {
 
-    public WebSocketMessageHandler() {
+    private final QuizService quizService;
+
+    public WebSocketMessageHandler(QuizService quizService) {
+        this.quizService = quizService;
     }
 
     @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
-        System.out.println(message.getPayload());
-        session.sendMessage(new TextMessage("Received: " + message.getPayload()));
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) {
+        String payload = message.getPayload();
+        quizService.processMessage(session, payload);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        quizService.handleDisconnect(session);
     }
 }
